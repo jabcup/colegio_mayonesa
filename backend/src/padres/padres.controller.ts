a6f73ed38da68f23c48333a62d8442174cfc6171
jh-desktop <joshernan.ferrari@gmail.com>
2025-12-07 16:23:34 -0400

    modulo de padres
    crear padre, listar padre, añadido setup y app.module.ts a gitignore

--- backend/src/padres/padres.controller.ts
+++ backend/src/padres/padres.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Get, Post, Controller } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { CreatePadreDto } from './dto/create-padre.dto';
+import { PadresService } from './padres.service';
 
 @Controller('padres')
-export class PadresController {}
+export class PadresController {
+  constructor(private readonly padresService: PadresService){}
+  @Post('CrearPadre')
+  @ApiOperation({ summary: 'Crear Padre' })
+  async createPadre(@Body() createPadreDto: CreatePadreDto){
+    const padre = await this.padresService.crear(createPadreDto);
+    return {
+      message: 'Padre creado correctamente',
+      padre,
+    }
+  }
+
+  @Get('MostrarPadres')
+  @ApiOperation({ summary: 'Mostrar padres' })
+  listarPadres(){
+    return this.padresService.todos();
+  }
+}
--- backend/src/padres/padres.service.ts
+++ backend/src/padres/padres.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Padres } from './padres.entity';
+import { CreatePadreDto } from './dto/create-padre.dto';
 
 @Injectable()
-export class PadresService {}
+export class PadresService {
+  constructor(
+    @InjectRepository(Padres)
+    private repo: Repository<Padres>,
+  ) {}
+
+  async crear(dto: CreatePadreDto): Promise<Padres> {
+    return this.repo.save(this.repo.create(dto)); // inserta
+  }
+
+  async todos(): Promise<Padres[]> {
+    return this.repo.find(); // select * from padres
+  }
+}
