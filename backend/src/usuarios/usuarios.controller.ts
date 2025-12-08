cf6465554448fde21a4e55af2360a80b3a23722b
Chuma <Chuma@gmail.com>
2025-12-07 21:04:05 -0400

    Módulo de Autenticación - login

--- backend/src/usuarios/usuarios.controller.ts
+++ backend/src/usuarios/usuarios.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsuariosService } from './usuarios.service';
+import { LoginUsuarioDto } from './dto/login-usuarios.dto';
 
 @Controller('usuarios')
-export class UsuariosController {}
+export class UsuariosController {
+  constructor(private readonly usuarioService: UsuariosService) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginUsuarioDto) {
+    return this.usuarioService.login(
+      loginDto.correo_institucional,
+      loginDto.contrasena,
+    );
+  }
+}
--- backend/src/usuarios/usuarios.service.ts
+++ backend/src/usuarios/usuarios.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuarios } from './usuarios.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsuariosService {}
+export class UsuariosService {
+  constructor(
+    @InjectRepository(Usuarios)
+    private readonly usuarioRepository: Repository<Usuarios>,
+  ) {}
+
+  async login(correo_institucional: string, contrasena: string) {
+    const usuario = await this.usuarioRepository.findOne({
+      where: { correo_institucional },
+    });
+    if (!usuario) {
+      throw new UnauthorizedException('Credenciales incorrectas');
+    }
+    const passValido = await bcrypt.compare(contrasena, usuario.contrasena);
+    if (!passValido) {
+      throw new UnauthorizedException('Credenciales incorrectas');
+    }
+    return {
+      message: 'Inicio de sesión exitoso',
+      usuario: { id: usuario.id, correo: usuario.correo_institucional },
+    };
+  }
+}
