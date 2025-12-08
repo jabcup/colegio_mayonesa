94e4e2ad1be2994fb6255294463da08e59c10d73
Zeta <ezequiel.solorzano.aranibar@gmail.com>
2025-12-08 11:41:49 -0400

    Update y Deletes logicos de: Padres y Materias uwu

--- backend/src/materias/materias.service.ts
+++ backend/src/materias/materias.service.ts
@@ -1,18 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm'; 
-import { Repository } from 'typeorm'; 
-import { Materias } from './materias.entity'; 
-import { CreateMateriaDto } from './dto/create-materia.dto'; 
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Materias } from './materias.entity';
+import { CreateMateriaDto } from './dto/create-materia.dto';
 
 @Injectable()
 export class MateriasService {
-  constructor(@InjectRepository(Materias) private repo: Repository<Materias>,){}
+  constructor(@InjectRepository(Materias) private repo: Repository<Materias>) {}
 
-  async crearMateria(dto: CreateMateriaDto): Promise<Materias>{
+  async crearMateria(dto: CreateMateriaDto): Promise<Materias> {
     return this.repo.save(this.repo.create(dto));
   }
 
-  async listarMaterias(): Promise<Materias[]>{
+  async listarMaterias(): Promise<Materias[]> {
     return this.repo.find();
+  }
+
+  async updateMateria(
+    id: number,
+    dtoMateria: CreateMateriaDto,
+  ): Promise<Materias> {
+    const materia = await this.repo.findOne({ where: { id } });
+
+    if (!materia) {
+      throw new Error('Curso no encontrado');
+    }
+
+    // Mezcla los datos nuevos con los actuales
+    Object.assign(materia, dtoMateria);
+
+    return this.repo.save(materia);
+  }
+
+  async deleteMateria(id: number): Promise<Materias> {
+    const materia = await this.repo.findOne({ where: { id } });
+
+    if (!materia) {
+      throw new Error('Curso no encontrado');
+    }
+
+    materia.estado = 'inactivo';
+
+    return this.repo.save(materia);
   }
 }
--- backend/src/padres/padres.controller.ts
+++ backend/src/padres/padres.controller.ts
@@ -1,24 +1,44 @@
-import { Body, Get, Post, Controller } from '@nestjs/common';
+import {
+  Body,
+  Get,
+  Post,
+  Controller,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { CreatePadreDto } from './dto/create-padre.dto';
 import { PadresService } from './padres.service';
 
 @Controller('padres')
 export class PadresController {
-  constructor(private readonly padresService: PadresService){}
+  constructor(private readonly padresService: PadresService) {}
   @Post('CrearPadre')
   @ApiOperation({ summary: 'Crear  Padre' })
-  async createPadre(@Body() createPadreDto: CreatePadreDto){
+  async createPadre(@Body() createPadreDto: CreatePadreDto) {
     const padre = await this.padresService.crear(createPadreDto);
     return {
       message: 'Padre creado correctamente',
       padre,
-    }
+    };
   }
 
   @Get('MostrarPadres')
   @ApiOperation({ summary: 'Mostrar padres' })
-  listarPadres(){
+  listarPadres() {
     return this.padresService.todos();
+  }
+
+  @Put('EditarPadre/:id')
+  @ApiOperation({ summary: 'Editar Papi' })
+  updatePadre(@Param('id') id: number, @Body() dto: CreatePadreDto) {
+    return this.padresService.updatePadre(id, dto);
+  }
+
+  @Delete('EliminarPadre/:id')
+  @ApiOperation({ summary: 'Eliminar Papasito (lógico)' })
+  deleteCurso(@Param('id') id: number) {
+    return this.padresService.deletePadre(id);
   }
 }
