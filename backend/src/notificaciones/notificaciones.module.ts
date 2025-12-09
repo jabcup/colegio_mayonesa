fbd65e88c357a6fb06bc1edb570e05a6d9b7c1d5
Sharum-java <156401841+Sharum-java@users.noreply.github.com>
2025-12-08 23:05:48 -0400

    Crear, Mostrar, Actualizar y Eliminar Notificaciones

--- backend/src/notificaciones/notificaciones.module.ts
+++ backend/src/notificaciones/notificaciones.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotificacionesController } from './notificaciones.controller';
 import { NotificacionesService } from './notificaciones.service';
+import { Notificaciones } from './notificaciones.entity';
+import { Estudiante } from 'src/estudiante/estudiante.entity';
+import { Personal } from 'src/personal/personal.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Notificaciones, Estudiante, Personal])],
   controllers: [NotificacionesController],
   providers: [NotificacionesService]
 })
