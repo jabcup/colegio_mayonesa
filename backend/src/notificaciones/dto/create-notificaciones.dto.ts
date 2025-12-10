b31cd34d74f0342f2c9933675a5e7fb3ec041a0c
Sharum-java <156401841+Sharum-java@users.noreply.github.com>
2025-12-09 23:52:10 -0400

    Arreglado Notificaciones

--- backend/src/notificaciones/dto/create-notificaciones.dto.ts
+++ backend/src/notificaciones/dto/create-notificaciones.dto.ts
@@ -1,12 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateNotificacionesDto {
     @ApiProperty({
         example: 1,
-        description: 'ID del estudiante al que se le envía la notificación',
+        description: 'ID del estudiante que recibe la notificación',
     })
     @IsNotEmpty()
+    @Type(() => Number)
+    @IsNumber()
     idEstudiante: number;
 
     @ApiProperty({
@@ -14,6 +17,8 @@ export class CreateNotificacionesDto {
         description: 'ID del personal que envía la notificación',
     })
     @IsNotEmpty()
+    @Type(() => Number)
+    @IsNumber()
     idPersonal: number;
 
     @ApiProperty({
