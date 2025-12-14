af4df252e920f92f7dd69674285591bed4afbb56
Chuma <Chuma@gmail.com>
2025-12-14 15:07:19 -0400

    Módulo horarios

--- frontend/app/components/Navbar/navbar.tsx
+++ frontend/app/components/Navbar/navbar.tsx
@@ -6,20 +6,39 @@ import { Boton } from "../botones/botonNav";
 import LogoutButton from "../botones/logout";
 
 export default function Navbar() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                    Mayonesa
-                </Typography>
-                <Boton label="Estudiantes" color="success" size="medium" className="ml-2" onClick={() => router.push("/estudiante")} />
-                    <Boton label="Materias" color="success" size="medium" className="ml-2" onClick={() => router.push("/materias")} />
-                    <LogoutButton/>
-            </Toolbar>
-        </AppBar>
-        </>
-    );
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Mayonesa
+          </Typography>
+          <Boton
+            label="Estudiantes"
+            color="success"
+            size="medium"
+            className="ml-2"
+            onClick={() => router.push("/estudiante")}
+          />
+          <Boton
+            label="Materias"
+            color="success"
+            size="medium"
+            className="ml-2"
+            onClick={() => router.push("/materias")}
+          />
+          <Boton
+            label="Horarios"
+            color="success"
+            size="medium"
+            className="ml-2"
+            onClick={() => router.push("/horarios")}
+          />
+          <LogoutButton />
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
